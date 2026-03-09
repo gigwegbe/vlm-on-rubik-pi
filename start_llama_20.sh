6b35d8275d2af55d6c38b003fca50828a8108742
George Igwegbe <gigwegbe@gmail.com>
2026-03-09 17:25:11 +0200

    updated scripts

--- start_llama_20.sh
+++ start_llama_20.sh
@@ -2,7 +2,7 @@ llama-server \
   -m ./LFM2-VL-1.6B-Q4_0.gguf \
   --mmproj ./mmproj-LFM2-VL-1.6B-F16.gguf \
   -b 4 \
-  -c 1024 \
+  -c 1048 \
   --threads 6 \
   --host 0.0.0.0 \
   --port 9876  \
